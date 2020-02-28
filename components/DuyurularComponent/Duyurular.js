f2a83245855f08e6ad31ddbed45508caa3b5b959
Fahreddin <fahreddingolcuk@gmail.com>
2020-02-28 13:59:10 +0300

    last_master

--- components/DuyurularComponent/Duyurular.js
+++ components/DuyurularComponent/Duyurular.js
@@ -19,6 +19,7 @@ export default class Duyurular extends React.Component {
     }
     handlePressIn = () => {
         console.log("Presss");
+        console.log('dsadsa')
         //TODO MAKE ANIMATION
     }
     showModal = () => {
